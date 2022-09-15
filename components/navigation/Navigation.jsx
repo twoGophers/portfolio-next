9a11527c8de6a595a0940b0481b10e1b4615b31a
Vladimir <falinvlad93@rambler.ru>
2022-09-16 01:08:43 +0300

    fix target blank

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -30,7 +30,7 @@ export default function Navigation() {
         </a>
       </Link>     
       <div className="navigation__panel panel">
-        <a href="https://vk.com/id456864977" target="_blank">
+        <a href="https://vk.com/id456864977" target="_blank" rel="noreffer">
           <div className={iconHoverVk ? 'panel__icon' : 'panel__icon panel__icon-animate-vk'}
             onMouseOver={() => setIconHoverVk(false)}
             onMouseOut={() => setIconHoverVk(true)}
@@ -44,7 +44,7 @@ export default function Navigation() {
             </Image>
           </div>
         </a>
-        <a href="https://t.me/falinVladimir" target="_blank">
+        <a href="https://t.me/falinVladimir" target="_blank" rel="noreffer">
           <div className={iconHoverTelegram ? "panel__icon" : "panel__icon panel__icon-animate-telegram"}
             onMouseOver={() => setIconHoverTelegram(false)}
             onMouseOut={() => setIconHoverTelegram(true)}
@@ -57,7 +57,7 @@ export default function Navigation() {
             </Image>
           </div>
         </a>
-        <a href="mailto:falinvlad93@rambler.ru" target="_blank">
+        <a href="mailto:falinvlad93@rambler.ru" target="_blank" rel="noreffer">
           <div className={iconHoverEmail ? "panel__icon" : "panel__icon panel__icon-animate-email"}
             onMouseOver={() => setIconHoverEmail(false)}
             onMouseOut={() => setIconHoverEmail(true)}>
