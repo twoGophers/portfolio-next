675feaa2d6a55e39ec4e510407bfd7f0c255912f
Vladimir <falinvlad93@rambler.ru>
2022-11-06 22:49:46 +0300

    fix style fixed navigation

--- pages/main/index.js
+++ pages/main/index.js
@@ -26,7 +26,7 @@ export default function Main( {skills, projects, testProjects} ) {
         <title>basic information</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header style={{ position: 'sticky', background: '#181818'}}>
+      <header style={{ position: 'fixed', background: '#181818'}}>
         <Navigation />
       </header>
         <section className='basic'>
