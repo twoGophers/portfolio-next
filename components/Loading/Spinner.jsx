f3f40cb60db84a0b775caafbf61fcea144edb119
Vladimir <falinvlad93@rambler.ru>
2023-08-31 21:44:51 +0300

    fix

--- components/Loading/Spinner.jsx
+++ components/Loading/Spinner.jsx
@@ -9,7 +9,7 @@ export default function Spinner() {
         <div className='spinner'>
             <div className="spinner-img">
                 <Image src={SpinnerSvg} alt='spinner'></Image>
-                <div className='spinner-text'>Loading...</div> 
+                <div className='spinner-text'>Loading...</div>
             </div>
         </div>
     </>
